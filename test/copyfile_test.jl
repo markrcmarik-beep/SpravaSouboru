cfc2dca06a0ac9c85db3f1fb9487a617092e9a9c
Martin <markrcmarik@gmail.com>
2026-01-03 21:08:33 +0100

    sync funkcni

--- test/copyfile_test.jl
+++
@@ -1,2 +0,0 @@
-using SpravaSouboru
-SpravaSouboru.copyfile_preserve_times("menugui_test.jl", "a/menugui_test.jl")
--- test/sync_folders_test.jl
+++ test/sync_folders_test.jl
@@ -3,9 +3,10 @@ using SpravaSouboru
 
 cesta01 = dirname(@__FILE__) # Získá cestu k adresáři aktuálního souboru
 podslozka01 = "sync_folders_test" # Podadresář s testovacími soubory
+podslozka02 = "sync_folders2_test" # Podadresář s testovacími soubory
 
-cesta1 = joinpath(cesta01, podslozka01, "MATLAB1")
-cesta2 = joinpath(cesta01, podslozka01, "MATLAB2")
+cesta1 = joinpath(cesta01, podslozka01)
+cesta2 = joinpath(cesta01, podslozka02)
 sync_folders(
     cesta1,          # zdrojová složka
     cesta2;        # cílová složka
